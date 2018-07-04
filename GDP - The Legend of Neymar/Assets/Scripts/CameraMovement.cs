2287dc0c1e1d4ff4c98562e4d5f6b68a40d91b12
Joao Rabello <rabellois@hotmail.com>
2018-07-03 23:18:16 -0300

    Modificações no cenário
    -Criado o interior da casa
    -Criado script que controla teleporte para regiões diferentes
    -Adicionada pasta para a build do jogo (executável)

--- GDP - The Legend of Neymar/Assets/Scripts/CameraMovement.cs
+++ GDP - The Legend of Neymar/Assets/Scripts/CameraMovement.cs
@@ -32,7 +32,7 @@ public class CameraMovement : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update () {
+    void FixedUpdate () {
         
         if (player.isAlive)
         {
---
+++ GDP - The Legend of Neymar/Assets/Scripts/ChangeAreaTP.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAreaTP : MonoBehaviour {
+
+    public Player player;
+    public Camera cam;
+
+    //public Vector3 firstWaypoint;
+    public Vector3 destinyWaypoint;
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            
+            player.transform.position = destinyWaypoint;
+            cam.transform.position = destinyWaypoint;
+        }
+    }
+}
